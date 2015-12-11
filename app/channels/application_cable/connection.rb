f4d2098662e9e5a9e22b25c88181833993e60bf5
Alec Tower <alectower@gmail.com>
2015-12-11 12:00:46 -0500

    Try and associate user with connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,19 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-#   identified_by :current_user
+    identified_by :current_user
 
     def connect
-#     self.current_user = User.first
+      self.current_user = find_verified_user
     end
 
-#    protected
-#
-#    def find_verified_user
-#      if current_user = User.find(cookies.signed[:user_id])
-#        current_user
-#      else
-#        reject_unauthorized_connection
-#      end
-#    end
+    protected
+
+    def find_verified_user
+      p cookies.signed[:user_id]
+      if current_user = User.find(cookies.signed[:user_id])
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
