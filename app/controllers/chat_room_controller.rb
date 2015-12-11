0e5e1a7c403baca6432fe0eae04b6bf895fd8449
Alec Tower <alectower@gmail.com>
2015-12-11 11:59:14 -0500

    Authorize access to chat

--- app/controllers/chat_room_controller.rb
+++ app/controllers/chat_room_controller.rb
@@ -1,4 +1,12 @@
 class ChatRoomController < ApplicationController
+  before_filter :authorize
+
   def index
+  end
+
+  private
+
+  def authorize
+    redirect_to new_session_path unless current_user
   end
 end
